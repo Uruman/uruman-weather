eed7a7c13dcd53d9d147e5ea65e8fe1fed304bab
MANUEL SPIES <SPIESM4@basfad.basf.net>
2020-07-24 10:23:04 +0200

    New forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=adaddaaa9b74c6abd2c6b861b329d566&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=adaddaaa9b74c6abd2c6b861b329d566&query=' + latitude + ',' + longitude
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to the weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees and there is a " + body.current.precip + "% chance to rain.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees with " + body.current.humidity + "% humidity. It feels actually like " + body.current.feelslike + " degrees.")
         }
     })
 }
